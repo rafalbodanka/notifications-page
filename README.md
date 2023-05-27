3215a1ef2c4db8c085cfee02d6a805779a768d2b
rafalbodanka <rafal.bodanka@gmail.com>
2023-05-27 19:32:48 +0200

    complete the challenge

---
+++ README.md
@@ -0,0 +1,37 @@
+# Frontend Mentor - Notifications page
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+- [My process](#my-process)
+  - [What I learned](#what-i-learned)
+  - [Possible upgrades](#possible-upgrades)
+
+## Overview
+
+### The challenge
+
+The challenge concerned building out this notifications page and getting it looking as close to the design as possible.
+
+### Screenshots
+
+![](./screenshot-desktop.png)
+![](./screenshot-mobile.png)
+
+### Links
+
+Solution URL: 
+
+## My process
+
+### What I learned
+
+During this challenge, I improved my grid and transform workaround.
+
+### Possible upgrades
+
+ - "Unreading" the message/all messages.
+ - On hover user's profile shortcut.
---
+++ script.js
@@ -0,0 +1,50 @@
+const markAsReadBtn = document.querySelector('.mark_as_read_btn')
+const notifications = document.querySelectorAll('.notification')
+const notifications_count = document.querySelector('.title-notification_number')
+
+const markAsRead = (notification) => {
+    notification.setAttribute('class', 'notification');
+    notification.querySelector('.notification_status').innerHTML = "";
+    checkUnreadMessages();
+};
+
+const markAllAsRead = () => {
+    const unreadMessages = document.querySelectorAll('.unread')
+    if (unreadMessages.length > 0) {
+        unreadMessages.forEach(message => {
+            markAsRead(message);
+        })
+    }
+    checkUnreadMessages();
+}
+
+const checkUnreadMessages = () => {
+    const unreadMessages = document.querySelectorAll('.unread')
+    if (unreadMessages.length > 0) {
+        markAsReadBtn.style.cursor = 'pointer';
+        markAsReadBtn.style.color = 'hsl(224, 21%, 14%)';
+        markAsReadBtn.addEventListener('click', markAllAsRead)
+    } else {
+        markAsReadBtn.style.cursor = 'default';
+        markAsReadBtn.style.color = 'gray';
+        markAsReadBtn.removeEventListener('click', markAllAsRead)
+        notifications_count.style.visibility = 'hidden';
+    }
+    notifications_count.innerHTML = unreadMessages.length
+}
+
+//first of all check if there are any unread messages to switch on the button
+checkUnreadMessages();
+
+//add event listeners for unread notifications
+notifications.forEach(notification => {
+    if (notification.hasAttribute('class', 'unread')) {
+        const clickHandler = () => {
+            markAsRead(notification);
+            notification.removeEventListener('click', clickHandler);
+        };
+        notification.addEventListener('click', clickHandler);
+    }
+});
+
+
